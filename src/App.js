cd31b540a72f0bd2a83d9aadd17c4102aafa35d2
장재영 <jangjy0105@gmail.com>
2023-05-18 13:29:09 +0900

    첫커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  
+  const [a, setA] = useState('a');
+
+  useEffect(() => {
+    axios.get('/api/hello') 
+    .then(res => setA(res.data))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {a}
     </div>
   );
 }
